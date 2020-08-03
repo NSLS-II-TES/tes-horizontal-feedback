03e4d303a2753a13a4e1c5b733f25f42802b4e22
Dan Allan <dallan@bnl.gov>
2020-08-03 16:33:17 -0400

    First draft of feedback plan.

--- horizontal_feedback.py
+++ horizontal_feedback.py
@@ -5,7 +5,9 @@ import functools
 import sys
 
 from bluesky import RunEngine
+from bluesky.plan_stubs import mvr, read, sleep
 from ophyd import EpicsMotor, EpicsSignal
+
 # from bluesky.log import config_bluesky_logging
 
 
@@ -26,22 +28,75 @@ print(f"inboard signal {inb_initial:.6}")
 print(f"outboard signal {outb_initial:.6}")
 BPMpos = (outb_initial - inb_initial) / (outb_initial + inb_initial) * 1000000
 print(f"position {BPMpos:.4}")
-source_pos = EpicsSignal("SR:APHLA:SOFB{BUMP:C08-BMB}offset:X-I",
-                         name="source_pos")
+source_pos = EpicsSignal("SR:APHLA:SOFB{BUMP:C08-BMB}offset:X-I", name="source_pos")
 print(f"source position {source_pos.get():.6}")
 print(datetime.now().isoformat())
 
 
 def plan(FBref):
-    yield from []
+    # The loop variable, FBi, is changed from within the loop.
+    for FBi in range(90, 101):
+        pico_value = yield from read(pm)["pm"]["value"]
+        outb_value = yield from read(outb)["outb"]["value"]
+        inb_value = yield from read(inb)["inb"]["value"]
+        FACTOR = 1000000  # TODO What is this, a unit conversion?
+        BPMpos = (outb_value - inb_value) / (inb_value + outb_value) * FACTOR
+        print(f"inboard signal {inb_value:.6}")
+        print(f"outboard signal {outb_value:.6}")
+        print(f"position {BPMpos:.4}    pico {pico_value:.6}")
+        deltaPos = BPMpos - FBref
+        # Chek limits.
+        FBi2 = 90
+        if pico_value - 1 < -550.0:
+            FBi = 101
+        else:
+            FBi = FBi2
+        if pico_value - 1 < 400.0:
+            FBi = 101
+        # check for no beam.
+        if (inb_value < 0.07) and (outb_value < 0.07):
+            FBi = 97
+        # compare and move as needed:
+        if FBi < 96:
+            if 20000 < deltaPos < 150000:
+                move_by = 0.021
+            elif -150000 < deltaPos < -25000:
+                move_by = -0.021
+            elif 150000 < deltaPos < 400000:
+                move_by = 0.063
+            elif -400000 < deltaPos < -150000:
+                move_by = -0.063
+            elif deltaPos > 400000:
+                move_by = 0.200
+            elif deltaPos < -400000:
+                move_by = -0.200
+            else:
+                move_by = 0
+        if move_by:
+            yield from mvr(pm, move_by)
+            print(f"moved {move_by} at {datetime.now().isoformat()}")
+        # set feedback frequency:
+        yield from sleep(0.125)
+        # if beam is lost, pause 30 sec and re-loop
+        if 96 < 99:
+            print(
+                "lost signal, waiting 30 sec starting at {datetime.now().isoformat()}"
+            )
+            yield from sleep(30)
+    yield from sleep(1.0)
+    pico_value = yield from read(pm)["pm"]["value"]
+    print(f"fedback sopped...     pico  {pico_value}")
+    print(f"inboard signal {inb_value:.6}")
+    print(f"outboard signal {outb_value:.6}")
+    print(f"position {BPMpos:.4}")
+    print(f"Plan exited at {datetime.now().isoformat()}")
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='TES horizontal feedback')
+    parser = argparse.ArgumentParser(description="TES horizontal feedback")
     parser.add_argument(
-        'FBref', type=float
-        help="Target position. For example, -137000.")
+        "FBref", type=float, help="Target position. For example, -137000."
+    )
     args = parser.parse_args()
     # config_bluesky_logging(level='INFO')
 
@@ -49,5 +104,5 @@ def main():
     RE(plan(args.FBref))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -4,4 +4,4 @@ import subprocess
 def test_cli_help():
     "Test that the CLI starts up without errors."
     # This raises CalledProcessError if it exits with a nonzero exit code.
-    subprocess.check_call(['python', 'horizontal_feedback.py', '-h'])
+    subprocess.check_call(["python", "horizontal_feedback.py", "-h"])
