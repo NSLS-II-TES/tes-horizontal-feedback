09adaa4f5cfaaf51843f8766ccc08bdd4bb200e6
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-05 17:32:59 -0400

    Apply fixes after testing on the beamline

--- horizontal_feedback.py
+++ horizontal_feedback.py
@@ -35,23 +35,25 @@ print(datetime.now().isoformat())
 
 def plan(FBref):
     # The loop variable, FBi, is changed from within the loop.
-    for FBi in range(90, 101):
-        pico_value = yield from read(pm)["pm"]["value"]
-        outb_value = yield from read(outb)["outb"]["value"]
-        inb_value = yield from read(inb)["inb"]["value"]
+    FBi = 90
+    while 90 <= FBi <= 100:
+        print(f'FBi: {FBi}')
+        pico_value = (yield from read(pm))["pm"]["value"]
+        outb_value = (yield from read(outb))["outb"]["value"]
+        inb_value = (yield from read(inb))["inb"]["value"]
         FACTOR = 1000000  # TODO What is this, a unit conversion?
         BPMpos = (outb_value - inb_value) / (inb_value + outb_value) * FACTOR
         print(f"inboard signal {inb_value:.6}")
         print(f"outboard signal {outb_value:.6}")
         print(f"position {BPMpos:.4}    pico {pico_value:.6}")
         deltaPos = BPMpos - FBref
-        # Chek limits.
+        # Check limits.
         FBi2 = 90
         if pico_value - 1 < -550.0:
             FBi = 101
         else:
             FBi = FBi2
-        if pico_value - 1 < 400.0:
+        if pico_value - 1 > 400.0:
             FBi = 101
         # check for no beam.
         if (inb_value < 0.07) and (outb_value < 0.07):
@@ -72,23 +74,25 @@ def plan(FBref):
                 move_by = -0.200
             else:
                 move_by = 0
+        else:
+            move_by = 0
         if move_by:
             yield from mvr(pm, move_by)
             print(f"moved {move_by} at {datetime.now().isoformat()}")
         # set feedback frequency:
-        yield from sleep(0.125)
-        # if beam is lost, pause 30 sec and re-loop
-        if 96 < 99:
+        yield from sleep(0.05)  # 20 Hz
+        # if beam is lost, pause 10 sec and re-loop
+        if FBi == 97:
             print(
-                "lost signal, waiting 30 sec starting at {datetime.now().isoformat()}"
+                f"lost signal, waiting 10 sec starting at {datetime.now().isoformat()}"
             )
-            yield from sleep(30)
+            yield from sleep(10)
     yield from sleep(1.0)
-    pico_value = yield from read(pm)["pm"]["value"]
-    print(f"fedback sopped...     pico  {pico_value}")
-    print(f"inboard signal {inb_value:.6}")
+    pico_value = (yield from read(pm))["pm"]["value"]
+    print(f"feedback stopped...     pico  {pico_value}")
+    print(f"inboard signal  {inb_value:.6}")
     print(f"outboard signal {outb_value:.6}")
-    print(f"position {BPMpos:.4}")
+    print(f"position        {BPMpos:.4}")
     print(f"Plan exited at {datetime.now().isoformat()}")
 
 
